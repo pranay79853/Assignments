05868925c85b248039cd6caa08a8798f0eef359b
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-01 14:04:21 +0530

    Data types in python

---
+++ Congratulations_message.py
@@ -0,0 +1,8 @@
+#Input from user (in lowercase)
+message = input("Enter your message: ")
+
+#Convert to uppercase
+upper_message = message.upper()
+
+#Print the result
+print("Converted message:", upper_message)
