b21d933b1262b043cc1d5abbbd0497a1922ee91e
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-06 12:24:01 +0530

    Python Operators - 1

---
+++ Square_root.py
@@ -0,0 +1,10 @@
+import math
+
+# Take input from user
+num = float(input("Enter a number: "))
+
+# Calculate square root
+result = math.sqrt(num)
+
+# Display result
+print("The square root of", num, "is", result)
