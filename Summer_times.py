4c932af8c22cbd4ae550811bcaf6c40ae39339a1
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-07 12:23:08 +0530

    Conditional Statements

---
+++ Summer_times.py
@@ -0,0 +1,10 @@
+# Program to check whether a number is positive, negative, or zero
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is Positive.")
+elif number < 0:
+    print("The number is Negative.")
+else:
+    print("The number is Zero.")
