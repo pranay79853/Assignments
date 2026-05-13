15c6bfec0dca90ab1ed492288a50f450a8f1512f
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-13 11:09:18 +0530

    Loops

---
+++ Power_calculator.py
@@ -0,0 +1,8 @@
+# Program to calculate n-th power of a number
+
+base = int(input("Enter the base number: "))
+power = int(input("Enter the power: "))
+
+result = base ** power
+
+print("Result =", result)
