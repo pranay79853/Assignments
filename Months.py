d363629f86a6896caf7604862397678081871222
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-23 11:08:00 +0530

    Python Coding Test - 2

---
+++ Months.py
@@ -0,0 +1,12 @@
+# Program to display all the month's names
+
+months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+]
+
+print("Months of the year are:")
+
+for month in months:
+    print(month)
