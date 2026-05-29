46f9c464c524f5a1a93b9c93e627c5e7cfc51d54
Pranay Rapolu <pranay.rapolu25@gmail.com>
2026-05-29 11:03:05 +0530

    Sets and Arrays

---
+++ Set_Symmetric_Difference.py
@@ -0,0 +1,10 @@
+# Python program to find the symmetric difference between two sets
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Finding symmetric difference
+result = set1.symmetric_difference(set2)
+
+# Display result
+print("Symmetric Difference:", result)
